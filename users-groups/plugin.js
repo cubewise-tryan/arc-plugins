df9d6a8c61014b62e01e10d420f5ed9f4cc0b9be
gpool21 <gpool@cubewise.com>
2018-05-01 17:04:31 +0200

    Update UserTab

--- users-groups/plugin.js
+++ users-groups/plugin.js
@@ -23,7 +23,7 @@ arc.directive("usersGroups", function () {
         link: function ($scope, element, attrs) {
 
         },
-        controller: ["$scope", "$rootScope", "$http", "$timeout", "$tm1", "$dialogs", "$helper","$log", function ($scope, $rootScope, $http, $timeout, $tm1, $dialogs, $helper,$log) {
+        controller: ["$scope", "$rootScope", "$http", "$timeout", "$tm1", "$dialogs", "$helper","$log","$q","$uibModal", function ($scope, $rootScope, $http, $timeout, $tm1, $dialogs, $helper,$log,$q,$uibModal) {
 
             // CHECK
             $log.log('testing123');
@@ -38,75 +38,147 @@ arc.directive("usersGroups", function () {
             //test users object
             $scope.users = [
                 {
-                    principalName:'testUser1',
-                    alias:'Mr Black',
-                    groups:'testGroup1',
-                    userLevel:'testLevel1'
+                    Name:'testUser1',
+                    FriendlyName:'Mr Black',
+                    Type:'testLevel1',
+                    IsActive: 'false',
+                    Groups:'testGroup1'
                 },
                 {
-                    principalName:'testUser2',
-                    alias:'Mr Pink',
-                    groups:'testGroup2',
-                    userLevel:'testLevel2'
+                    Name:'testUser2',
+                    FriendlyName:'Mr Pink',
+                    Type:'testLevel2',
+                    IsActive: 'false',
+                    Groups:'testGroup2'
                 },
                 {
-                    principalName:'testUser3',
-                    alias:'Mr White',
-                    groups:'testGroup3',
-                    userLevel:'testLevel3'
+                    Name:'testUser3',
+                    FriendlyName:'Mr White',
+                    Type:'testLevel3',
+                    IsActive: 'false',
+                    Groups:'testGroup3'
                 },
                 {
-                    principalName:'testUser4',
-                    alias:'Mr Orange',
+                    Name:'testUser4',
+                    FriendlyName:'Mr Orange',
                     groups:'testGroup4',
-                    userLevel:'testLevel4'
-                },
+                    Type:'testLevel4',
+                    IsActive: 'false',
+                    Groups:'testGroup4'
+                }
 
             ];
 
             //LOAD USER DATA
-            // var load = function(){
-                // $scope.reload = false;
-                // Loads the thread information from TM1
+            var load = function(){
+                // Loads the UserList information from TM1
                 // First part of URL is the encoded instance name and then REST API URL (excluding api/v1/)
-                // $http.get(encodeURIComponent($scope.instance) + "/Threads").then(function(success, error) {
-                    // if(success.status == 401){
-                        // Set reload to true to refresh after the user logs in
-                        // $scope.reload = true;
-                        // return;
-                    // }
-                    // else if(success.status < 400){	
-                        // Set the threads data		
-                        // $scope.now = new Date();
-                        // $scope.threads = success.data.value;
-                        // _.each($scope.threads, function(item){
-                            // item.canCancel = item.State != "Idle";
-                            // item.ElapsedTimeS = $helper.timespanToSeconds(item.ElapsedTime);
-                            // item.WaitTimeS = $helper.timespanToSeconds(item.WaitTime);
-                        // })
-                        // if(!$scope.isPaused){
-                            // If the page is active retrieve the data again in 1 second
-                            // $scope.loadTimeout = setTimeout(function(){
-                                // load();
-                            // }, $rootScope.uiPrefs.sessionInterval);
-                        // }         
-                    // } else {
-                        // Error to display
-                        // if(success.data && success.data.error && success.data.error.message){
-                            // $scope.message = success.data.error.message;
-                        // }
-                        // else {
-                            // $scope.message = success.data;
-                        // }
-                        // $timeout(function(){
-                            // $scope.message = null;
-                        // }, 5000);
-                    // }
-                // });
-            // };
 
-            // Load the threads the first time
-            // load();
+                //check
+                $log.log('in load function>>>');
+
+                //set reaload flag
+                $scope.reload = false;
+
+                //TM1 REST API QUERY
+                var odataQuery = "/Users";
+
+                //MAIN FUNCTION
+                $http.get(encodeURIComponent($scope.instance) + odataQuery)
+                    .then(success)
+                    .catch(error);
+
+                    //SUCCESS
+                    function success(response){
+                        //review status
+                        if(response.status < 400){
+                            //check
+                            $log.log('success');
+                            $log.log(response.data.value);
+
+                            //build user object........
+                            buildUsers(response.data.value);
+
+                        }else{
+                            return $q.reject(response);
+                        }
+
+
+                        //construct user object
+                        function buildUsers(usersArray){
+
+                            $scope.users = usersArray;
+                            $log.log($scope.users);
+                            // angular.forEach(usersArray, function(value,key){
+                                // $log.log(value);
+                                // $log.log(key);
+                            // });
+                        }
+
+                    }
+
+                    //ERRORHANDLER
+                    function error(response){
+                        //check
+                        $log.log('error');
+                        $log.log(response);
+
+                        if(response == 400){
+                            // Set reload to true to refresh after the user logs in
+                            $scope.reload = true;
+                            return;
+
+                        }else{
+                            // Error to display on page
+                            if(response.data && response.data.error && response.data.error.message){
+                                $scope.message = success.data.error.message;
+                            }
+                            else {
+                                $scope.message = success.data;
+                            }
+                            $timeout(function(){
+                                $scope.message = null;
+                            }, 5000);
+
+                        }
+                        
+
+                    }
+
+            };
+
+            // Load the users the first time
+            load();
+
+
+            //EDIT USER
+            var $ctrl = this;
+            $ctrl.items = ['item1', 'item2', 'item3'];
+            
+            $scope.editUserModal = function(){
+                //check
+                $log.log('editUser');
+
+                var modalInstance = $uibModal.open({
+                    animation: $ctrl.animationsEnabled,
+                    component: 'modalComponent',
+                    resolve: {
+                        items: function () {
+                            return $ctrl.items;
+                        }
+                    }
+                });
+
+            }
+
+            //RELOAD FOR SESSION TIME OUTS
+            // Event to reload the page, normally after the session has timed out
+            $scope.$on("login-reload", function(event, args) {
+                // Check that instance in args matches your $scope
+                if(args.instance === $scope.instance && $scope.reload){
+                    load();
+                }
+            });
 
 
             //FILTER USER TABLE
@@ -118,8 +190,8 @@ arc.directive("usersGroups", function () {
 
                 //Check for Match
                 var filter = $scope.selections.filter.toLowerCase();
-                if(user.principalName.toLowerCase().indexOf(filter) !== -1
-                    || user.alias.toLowerCase().indexOf(filter) !== -1 ){
+                if(user.Name.toLowerCase().indexOf(filter) !== -1
+                    || user.FriendlyName.toLowerCase().indexOf(filter) !== -1 ){
                     return true;
                 }
                 return false;
@@ -140,141 +212,12 @@ arc.directive("usersGroups", function () {
                 }
             });
 
+
             //CLEANUP - destroy event broadcast just before child scope removal..
             $scope.$on("$destroy", function(event){
                 // Cancel the timeout and any other resources
                 clearTimeout($scope.loadTimeout);
             });
-
-
-            //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> REFERENCE
-            // $scope.isPaused = false;
-            // $scope.activeOnly = false;
-            // $scope.now = new Date();
-            // $scope.selections = {
-                // filter: ""
-            // };
-
-            // if(!$rootScope.uiPrefs.sessionInterval){
-                // Set the session interval if it doesn't exist
-                // uiPrefs is stored in the browser
-                // $rootScope.uiPrefs.sessionInterval = 1000;
-            // }
-
-            // var load = function(){
-                // $scope.reload = false;
-                // Loads the thread information from TM1
-                // First part of URL is the encoded instance name and then REST API URL (excluding api/v1/)
-                // $http.get(encodeURIComponent($scope.instance) + "/Threads").then(function(success, error) {
-                    // if(success.status == 401){
-                        // Set reload to true to refresh after the user logs in
-                        // $scope.reload = true;
-                        // return;
-                    // }
-                    // else if(success.status < 400){	
-                        // Set the threads data		
-                        // $scope.now = new Date();
-                        // $scope.threads = success.data.value;
-                        // _.each($scope.threads, function(item){
-                            // item.canCancel = item.State != "Idle";
-                            // item.ElapsedTimeS = $helper.timespanToSeconds(item.ElapsedTime);
-                            // item.WaitTimeS = $helper.timespanToSeconds(item.WaitTime);
-                        // })
-                        // if(!$scope.isPaused){
-                            // If the page is active retrieve the data again in 1 second
-                            // $scope.loadTimeout = setTimeout(function(){
-                                // load();
-                            // }, $rootScope.uiPrefs.sessionInterval);
-                        // }         
-                    // } else {
-                        // Error to display
-                        // if(success.data && success.data.error && success.data.error.message){
-                            // $scope.message = success.data.error.message;
-                        // }
-                        // else {
-                            // $scope.message = success.data;
-                        // }
-                        // $timeout(function(){
-                            // $scope.message = null;
-                        // }, 5000);
-                    // }
-                // });
-            // };
-
-            // Load the threads the first time
-            // load();
-
-            // $scope.togglePaused = function(){
-                // $scope.isPaused = !$scope.isPaused;
-                // if(!$scope.isPaused){
-                    // Starting loading again
-                    // load();
-                // }
-            // };
-
-            // $scope.toggleActiveOnly = function(){
-                // $scope.activeOnly = !$scope.activeOnly;
-            // };
-
-            // $scope.listFilter = function(item) {
-                // If active only hide Idle threads
-                // if($scope.activeOnly && item.State == "Idle"){
-                    // return false;
-                // }
-                // Check text filter
-                // if(!$scope.selections.filter || !$scope.selections.filter.length){
-                    // return true;
-                // }
-                // var filter = $scope.selections.filter.toLowerCase();
-                // if(item.Name.toLowerCase().indexOf(filter) !== -1 || item.Function.toLowerCase().indexOf(filter) !== -1 || item.ObjectName.toLowerCase().indexOf(filter) !== -1){
-                    // return true;
-                // }
-                // return false;
-            // };
-
-            // $scope.cancelThread = function(id){
-
-                // $http.post(encodeURIComponent($scope.instance) + "/Threads(" + id + ")/tm1.CancelOperation",  {}).then(function(success, error) {
-                        // if(success.status == 401){
-                            // return;
-                        // } else if(success.status < 400){
-                            // Success the thread was cancelled
-                        // } else {
-                            // if(success.data && success.data.error && success.data.error.message){
-                                // $scope.message = success.data.error.message;
-                            // }
-                            // else {
-                                // $scope.message = success.data;
-                            // }
-                            // $timeout(function(){
-                                // $scope.message = null;
-                            // }, 5000);
-                        // }            
-                    // }
-                // );
-
-            // };
-
-            // $scope.$on("login-reload", function(event, args) {
-                // Event to reload the page, normally after the session has timed out
-                // Check that instance in args matches your $scope
-                // if(args.instance === $scope.instance && $scope.reload){
-                    // load();
-                // }
-            // });
-                
-            // $scope.$on("close-tab", function(event, args) {
-                // Event to capture when a user has clicked close on the tab
-                // if(args.page == "threads" && args.instance == $scope.instance && args.name == null){
-                    // The page matches this one so close it
-                    // $rootScope.close(args.page, {instance: $scope.instance});
-                // }
-            // });
-
-            // $scope.$on("$destroy", function(event){
-                // Cancel the timeout and any other resources
-                // clearTimeout($scope.loadTimeout);
-            // });
         
 
         }]
